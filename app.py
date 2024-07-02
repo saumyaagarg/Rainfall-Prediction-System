2d58e8dc357fc7db77d2a4ad411179733346e42e
Saumyaa Garg <129076462+saumyaagarg@users.noreply.github.com>
2024-07-02 18:54:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ if st.session_state.page == "Rainfall Prediction":
 
 # Home
 else:
-    img = "img5.jpg"
+    img = "img1.jpg"
     st.title("RainVision")
     st.write("##### Welcome to RainVision, your ultimate Rainfall Prediction App!")
     st.image(img, width=750)
